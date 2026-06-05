dd35ed99fe45a203477563513fe25865eec3f54e
Night-Swan <naren42651@gmail.com>
2026-06-05 14:23:03 +0200

    Create job

--- internal/db/db.go
+++ internal/db/db.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"time"
+    "context"
 )
 
 type Job struct {
@@ -16,3 +17,12 @@ type Job struct {
     JobType string `db:"job_type"`
 }
 
+func CreateJob(submittedBy string, jobType string, payload []byte) (int64, error) {
+    var id int64
+    err := Pool.QueryRow(context.Background(), `
+    INSERT INTO jobs (job_type, payload, submitted_by)
+    VALUES ($1, $2, $3)
+    RETURNING id
+    `, jobType, payload, submittedBy).Scan(&id)
+    return id, err
+}
